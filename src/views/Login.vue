36b0b7c8bf07ae901a9384ae9f585b1ac756ae79
dimalepel <dimalepel@mail.ru>
2022-05-18 00:23:13 +0300

    feat: vuelidate not work Vue 3. TODO: fix this other brinch

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="submitHandler">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
@@ -34,8 +34,23 @@
 
       <p class="center">
         Нет аккаунта?
-        <a href="/">Зарегистрироваться</a>
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
 </template>
+
+<script>
+export default {
+  name: 'login',
+  data: () => ({
+    email: '',
+    password: '',
+  }),
+  methods: {
+    submitHandler() {
+      this.$router.push('/');
+    },
+  },
+};
+</script>
