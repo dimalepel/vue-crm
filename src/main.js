cea55cf524412c2b0841cd0efc55828b2b895b26
dimalepel <dimalepel@mail.ru>
2022-05-19 23:31:14 +0300

    init: start lesson 7

--- src/main.js
+++ src/main.js
@@ -11,3 +11,12 @@ createApp(App)
   .use(store)
   .use(router)
   .mount('#app');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDcyTvoMFFiGXdyg6VjUimk4f-H9nVfnIo',
+  authDomain: 'vue-crm-51e0a.firebaseapp.com',
+  projectId: 'vue-crm-51e0a',
+  storageBucket: 'vue-crm-51e0a.appspot.com',
+  messagingSenderId: '666895786848',
+  appId: '1:666895786848:web:63c7d7c040c2a40f8cb405',
+};
