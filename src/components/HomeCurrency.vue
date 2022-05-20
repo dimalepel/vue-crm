38c07bc6adecf0a08516a2bd4a96a7e658eb1d99
dimalepel <dimalepel@mail.ru>
2022-05-20 13:28:16 +0300

    feat: finish lesson 9

---
+++ src/components/HomeCurrency.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="col s12 m6 l8">
+    <div class="card orange darken-3 bill-card">
+      <div class="card-content white-text">
+        <div class="card-header">
+          <span class="card-title">Курс валют</span>
+        </div>
+        <table>
+          <thead>
+          <tr>
+            <th>Валюта</th>
+            <th>Курс</th>
+            <th>Дата</th>
+          </tr>
+          </thead>
+
+          <tbody>
+          <tr
+            v-for="cur of currencies"
+            :key="cur"
+          >
+            <td>{{ cur }}</td>
+            <td>{{ rates[cur].toFixed(4) }}</td>
+            <td>{{ dateFilter('date') }}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'rates',
+    'date',
+  ],
+  data: () => ({
+    currencies: [
+      'BYN',
+      'RUB',
+      'USD',
+      'EUR',
+    ],
+  }),
+  methods: {
+    dateFilter(format = 'date') {
+      const options = {};
+
+      if (format.includes('date')) {
+        options.day = '2-digit';
+        options.month = 'long';
+        options.year = 'numeric';
+      }
+
+      if (format.includes('time')) {
+        options.hour = '2-digit';
+        options.minute = '2-digit';
+        options.second = '2-digit';
+      }
+
+      return new Intl.DateTimeFormat('ru-RU', options).format(new Date(this.date));
+    },
+  },
+};
+</script>
