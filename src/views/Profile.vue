18b6227ec2f13e1288de584d391e6f4c0ac2a391
dimalepel <dimalepel@mail.ru>
2022-05-31 16:32:40 +0300

    feat: add update user name lesson 19

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,15 +4,28 @@
       <h3>Профиль</h3>
     </div>
 
-    <form class="form">
+    <form class="form" @submit.prevent="submitHandler">
       <div class="input-field">
         <input
           id="description"
           type="text"
+          v-model="name"
+          :class="{ invalid: v$.name.$dirty && !v$.name.required.$response }"
         >
         <label for="description">Имя</label>
         <span
-          class="helper-text invalid">name</span>
+          class="helper-text invalid"
+          v-if="v$.name.$dirty && !v$.name.required.$response"
+        >Имя не должно быть пустым</span>
+      </div>
+
+      <div class="switch">
+        <label>
+          English
+          <input type="checkbox">
+          <span class="lever"></span>
+          Русский
+        </label>
       </div>
 
       <button class="btn waves-effect waves-light" type="submit">
@@ -22,3 +35,49 @@
     </form>
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+import useVuelidate from '@vuelidate/core';
+import { required } from '@vuelidate/validators';
+
+export default {
+  data: () => ({
+    v$: useVuelidate(),
+    name: '',
+  }),
+  validations: () => ({
+    name: { required },
+  }),
+  mounted() {
+    this.name = this.info.name;
+    setTimeout(() => {
+      M.updateTextFields();
+    });
+  },
+  computed: {
+    ...mapGetters(['info']),
+  },
+  methods: {
+    ...mapActions(['updateInfo']),
+    async submitHandler() {
+      const isFormCorrect = await this.v$.$validate();
+      if (!isFormCorrect) {
+        return;
+      }
+
+      try {
+        await this.updateInfo({
+          name: this.name,
+        });
+      } catch (e) {}
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .switch {
+    margin-bottom: 2rem;
+  }
+</style>
