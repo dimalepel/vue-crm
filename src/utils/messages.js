b3aef6dd8b43eeecfa213857ab85581de8b50f92
dimalepel <dimalepel@mail.ru>
2022-06-06 15:35:34 +0300

    fix: localize

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,7 +1,7 @@
 export default {
-  logout: 'LogoutMessage',
-  login: 'LoginMessage',
-  'auth/user-not-found': 'UserNotFound',
-  'auth/wrong-password': 'WrongPassword',
-  'auth/email-already-in-use': 'EmailAlreadyInUse',
+  logout: this.$filters.localizeFilter('LogoutMessage'),
+  login: this.$filters.localizeFilter('LoginMessage'),
+  'auth/user-not-found': this.$filters.localizeFilter('UserNotFound'),
+  'auth/wrong-password': this.$filters.localizeFilter('WrongPassword'),
+  'auth/email-already-in-use': this.$filters.localizeFilter('EmailAlreadyInUse'),
 };
