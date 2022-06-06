93541cddeea88f56e19b313af55bb4425e46bdcb
dimalepel <dimalepel@mail.ru>
2022-06-06 14:02:28 +0300

    feat: finish lesson 20

--- src/utils/message.plugin.js
+++ src/utils/message.plugin.js
@@ -1,12 +1,12 @@
 export default {
   install: (App) => {
     App.config.globalProperties.$message = function (html) {
-      M.toast({ html });
+      M.toast({ html: this.$filters.localizeFilter(html) });
     };
 
     App.config.globalProperties.$error = function (html) {
       M.toast({
-        html: `[Ошибка]: ${html}`,
+        html: `[${this.$filters.localizeFilter('Error')}]: ${this.$filters.localizeFilter(html)}`,
       });
     };
   },
